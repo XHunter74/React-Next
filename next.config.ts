96082e16a0a479bf818a273b326e1688d66b4e17
serhiy.krasovskyy <xhunter74@gmail.com>
2025-04-08 21:36:31 +0300

    Try to fix issue with static files.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: '/',
 };
 
 export default nextConfig;
